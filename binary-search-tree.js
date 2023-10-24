a8dcddb67e08c1c8b2d0ae8b8f2013ae09d8b071
zacharybaca <zachary.baca@msn.com>
2023-10-24 17:05:27 -0500

    complete

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -43,27 +43,67 @@ class BinarySearchTree {
 
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here
+    // Print the current node value
+    if (!currentNode) return null;
+    if (currentNode) console.log(currentNode.val);
+    // Recursively call the left subtree
+    this.preOrderTraversal(currentNode.left);
+    // Recursively call the right subtree
+    this.preOrderTraversal(currentNode.right);
   }
 
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here
+    if (!currentNode) return null;
+    //Recursively call the left subtree
+    this.inOrderTraversal(currentNode.left);
+    //Print the current node value
+    console.log(currentNode.val);
+    //Recursively call the right subtree
+    this.inOrderTraversal(currentNode.right);
   }
 
 
   postOrderTraversal(currentNode = this.root) {
-    // Your code here
+    if (!currentNode) return null;
+    //Recursively call the left subtree
+    this.postOrderTraversal(currentNode.left);
+    //Recursively call the right subtree
+    this.postOrderTraversal(currentNode.right);
+    //Print the current node value
+    console.log(currentNode.val);
   }
 
     // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
-    // your code here
+    if (!this.root) return null;
+    //Put the starting node in a queue
+    const queue = [this.root];
+    //While the queue is not empty, repeat steps 3-4
+    while (queue.length) {
+    //Dequeue a node and print it
+    let currNode = queue.shift();
+    console.log(currNode.val);
+    //Put all of the node's children in the back of the queue
+    if (currNode.left) queue.push(currNode.left);
+    if (currNode.right) queue.push(currNode.right);
+    }
   }
 
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
-    // your code here
+    if (!this.root) return null;
+    //Put the starting node on a STACK
+    const stack = [this.root];
+    //While the STACK is not empty, repeat steps 3-4
+    while (stack.length) {
+    //POP a node and print it
+    let currNode = stack.pop();
+    console.log(currNode.val);
+    //Put all of the node's children on the TOP of the STACK
+    if (currNode.left) stack.push(currNode.left);
+    if (currNode.right) stack.push(currNode.right);
+    }
 }
 }
 
